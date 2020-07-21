09353af0b1755e7b2041a8cf099bc4cdc7f87c3d
C. Chang <chloechang416@gmail.com>
2020-07-21 19:27:55 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,10 @@
 
+def introduction (name)
+  puts "Hi, my name is #{name}."
+end
+
+
+
 def introduction_to_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
